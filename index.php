5cc1ef56c88913bbc22d98e3f866e74590f54446
mosx <s.sivinyuk@gmail.com>
2018-01-30 16:58:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
+
+@date_default_timezone_set('Europe/London');
+
+define('XPATH', dirname(__FILE__) );
+    
+define('X_TEMPLATE', "default" );
+define( 'DS', DIRECTORY_SEPARATOR );
+require_once ( XPATH.DS.'Sources'.DS.'defines.php' );
+
+xload("class.lib.main");
+xload("class.lib.auth");
+xload("functions");
+xload("mainframe");
+
+$mainframe = new mainframe;
+
+$mainframe->run();
+?>
